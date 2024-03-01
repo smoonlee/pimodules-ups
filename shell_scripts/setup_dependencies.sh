62426c848eb68c823fcf43d7126544b69aa680be
Simon Lee <simon@leefamily.uk.net>
2024-03-01 17:38:21 +0000

    Update setup_dependencies.sh

--- shell_scripts/setup_dependencies.sh
+++ shell_scripts/setup_dependencies.sh
@@ -14,13 +14,13 @@
   # echo '--- rpi-update'
   # sudo rpi-update
   echo '--- install some packages'
-  sudo apt-get install -y git python-dev python-pip python-serial python-smbus python-jinja2 python-xmltodict python-psutil i2c-tools libi2c-dev
+  sudo apt-get install -y git i2c-tools libi2c-dev python3-dev python3-pip python3-serial python3-smbus python3-jinja2 python3-xmltodict python3-psutil 
   echo '--- pip install rpi.gpio'
-  sudo pip install RPi.GPIO
+  sudo pip3 install RPi.GPIO
   echo '--- pip install psutil'
-  sudo pip install psutil
+  sudo pip3 install psutil
   echo '--- pip install xmltodict'
-  sudo pip install xmltodict
+  sudo pip3 install xmltodict
   echo '--- all done, rebooting'
   sudo reboot
   exit 0
